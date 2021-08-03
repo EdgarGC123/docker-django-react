08db4257c87ee07274c42bbb62fd4d8a7087f15c
Edgar GC <Edgar.GalvanCuesta@gmail.com>
2021-08-03 11:31:06 -0500

    user passwords are now hashed, fully create path for user login

--- admin/users/views.py
+++ admin/users/views.py
@@ -1,24 +1,41 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import exceptions
+from rest_framework import exceptions, serializers
 from .models import User
+from .serializers import UserSerializer
 # import users.models import User
 
 # Create your views here.
 
 
-@api_view(['GET'])
-def registers(request):
+@api_view(['POST'])
+def register(request):
     data = request.data
 
     if data['password'] != data['password_confirm']:
         raise exceptions.APIException('Passwords do not match!')
 
-    serializer = userSerializer(data=data)
+    serializer = UserSerializer(data=data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    user = User.objects.filter(email=email).first()
+
+    if user is None:
+        raise exceptions.AuthenticationFailed("User Not Found!")
+
+    if not user.check_password(password):
+        raise exceptions.AuthenticationFailed("Incorrect Password!")
+
+    return Response('success')
 
 
 @api_view(['GET'])
