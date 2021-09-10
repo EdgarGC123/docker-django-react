e68a62c388381568d75f31c6da1f594235f5a2ab
edgar gc <edgar.galvancuesta@gmail.com>
2021-09-09 20:28:17 -0500

    Create chart view in Dashboard and import data via axios from django

--- react-admin/src/secure/dashboard/Dashboard.tsx
+++ react-admin/src/secure/dashboard/Dashboard.tsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Wrapper from '../Wrapper';
+import c3 from 'c3';
+import axios from 'axios';
 
-const Dashboard = () =>(
-    (
+class Dashboard extends Component {
+  componentDidMount = async () =>{
+    let chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        x: 'x',
+        columns: [
+          ['x', '2020-1-1', '2020-2-2'],
+          ['Sales', 100, 200],
+        ],
+        types:{
+          Sales: 'bar'
+        }
+
+      },
+      axis:{
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%d-%m-%Y'
+          }
+        }
+      }
+    });
+
+    const response = await axios.get('chart');
+
+    const records: {date: string, sum: number}[] = response.data.data;
+
+    console.log(records)
+
+    chart.load({
+      columns:[
+        ['x', ...records.map(record => record.date)],
+        ['Sales', ...records.map(record => record.sum)]
+      ]
+    })
+  }
+  
+  render() {
+    return (
       <Wrapper>
-        Dashboard
+        <h2>Daily Sales</h2>
+        <div id="chart"/>
       </Wrapper>
     )
-)
+  }
+}
 
-
-export default Dashboard
+export default Dashboard;
